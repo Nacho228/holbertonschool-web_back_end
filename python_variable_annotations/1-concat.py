ec04a984eacca5e33a62f18f0d6e480154fd303c
Nacho228 <6319@holbertonstudents.com>
2023-08-28 08:56:07 -0700

    1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" type-annotated function concat that takes a
+string str1 and a string str2 as arguments"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Concatenates two strings.
+    Params:
+    str1: first string.
+    str2: second string.
+    Return: String.
+    """
+    return (str1 + str2)
