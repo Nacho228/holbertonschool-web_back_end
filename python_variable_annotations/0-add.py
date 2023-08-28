4a513f35bcd63eb6f9fc59813572ed95d905c9de
Nacho228 <6319@holbertonstudents.com>
2023-08-28 08:48:43 -0700

    0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -4,4 +4,10 @@ adds two float """
 
 
 def add(a: float, b: float) -> float:
+    """ Function that adds two float
+        Params:
+        a (float): first number to be added.
+        b (float): second number to be added.
+        Return: float
+    """
     return (a + b)
