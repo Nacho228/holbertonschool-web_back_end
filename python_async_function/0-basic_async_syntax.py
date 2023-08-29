16fc036b319ec8291bee735819575549f398ca07
Nacho228 <6319@holbertonstudents.com>
2023-08-29 04:25:42 -0700

    0-basic_async_syntax.py

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Asynchronous coroutine that takes in an integer argument
+(max_delay, with a default value of 10) named wait_random
+that waits for a random delay between 0 and max_delay
+(included and float value) seconds and eventually returns it."""
+from typing import Union
+import asyncio
+import random
+
+
+async def wait_random(max_delay: Union[int, float] = 10):
+    """ """
+    rndm = random.uniform(0, max_delay)
+    await asyncio.sleep(rndm)
+    return (rndm)
