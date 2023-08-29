8460179c378aee10011eb893b1300e08f0066b11
Nacho228 <6319@holbertonstudents.com>
2023-08-29 05:37:38 -0700

    both one and two fixed

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -9,7 +9,10 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """ """
+    """
+    Waits for a random delay between 0 and max_delay
+    (included and float value) seconds and eventually returns it.
+    """
     rndm = random.uniform(0, max_delay)
     await asyncio.sleep(rndm)
     return (rndm)
--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,9 +8,10 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Wait for a random delay between 0
-    and max_delay(included and float value)
-    seconds and eventually returns it."""
+    """
+    Function that spawn wait_random n
+    times with the specified max_delay.
+    """
     tasks = []
     delays = []
     for _ in range(n):
