0d4e0ba81a170e55d7effe55df5c7212c66ac62b
Nacho228 <6319@holbertonstudents.com>
2023-09-08 13:37:12 -0700

    10-update_topics.py

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Task 10 """
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Function that changes all topics of a
+    school document based on the name
+    Args:
+        mongo_collection: pymongo collection object
+        name: str: will be the school name to update
+        topics: list[str]: will be the list of topics approached in the school
+    Return:
+        The changed document
+    """
+    result = mongo_collection.update_one(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+    return result.modified_count
