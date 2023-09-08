9e5bdc79e1921acec36e98d1a1cdee9e4790b3e4
Nacho228 <6319@holbertonstudents.com>
2023-09-08 11:40:02 -0700

    8-all.py

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -3,6 +3,14 @@
 
 
 def list_all(mongo_collection):
+    """
+    Python function that lists,
+    all documents in a collection
+    Params:
+    mongo_collection: pymongo collection object
+    Returns:
+    list of dictionaries with the data from each document.
+    """
     cursor = mongo_collection.find({})
 
     data_list = []
