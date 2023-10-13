9ec1b57c9499325647075c59db0c769dde8fcd8d
Nacho228 <6319@holbertonstudents.com>
2023-10-13 09:57:51 -0700

    0-promise.js

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,4 @@
-// Keep every promise you make and only make promises you can keep
 export default function getResponseFromAPI() {
-    const promise = Promise.resolve();
-    return promise;
-};
+  const promise = Promise.resolve();
+  return promise;
+}
